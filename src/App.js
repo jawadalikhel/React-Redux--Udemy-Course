49491f03b22455865dc44aa2d50f23eebc760dda
Your Name <jawadalikhel18@gmail.com>
2019-01-03 10:14:16 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,127 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person.js';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      persons: [
+        {name: 'Redwan', age: 18},
+        {name: 'Manu', age: 29},
+        {name: 'jawad', age: 24},
+      ],
+      showall: false,
+      hideUserOne: true,
+      hideUserTwo: true,
+      hideUserThree: true,
+    }
+  }
+
+  switchNameHandler = (newName) =>{
+    this.setState({
+      persons: [
+        {name: 'Redwan', age: 18},
+        {name: 'Manu', age: 29},
+        {name: newName, age: 24},
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) =>{
+    this.setState({
+      persons: [
+        {name: 'Redwan', age: 18},
+        {name: event.target.value, age: 29},
+        {name: 'jawad', age: 24},
+      ]
+    })
+  }
+
+  show = () =>{
+    const switchShowall = this.state.showall;
+    this.setState({
+      showall: !switchShowall
+    })
+  }
+
+  render() {
+
+    const style = {
+      backgroundColor: 'red',
+      font: 'inherit',
+      color: 'white',
+      border: '1px solid blue',
+      padding: '8px',
+    }
+
+    let people = null;
+    if(this.state.showall === true){
+      people = (
+        <div>
+          <Person click={this.switchNameHandler.bind(this, 'dawaj')} name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+          <Person changed={this.nameChangedHandler} name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+
+      {/* what is .bind........... learn more about it */}
+
+        <button style={style} onClick={this.show}>Show All</button>
+        {people}
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+// userOne = () =>{
+//   const deltOne = this.state.hideUserOne;
+//   this.setState({
+//     hideUserOne: ! deltOne,
+//   })
+// }
+//
+// userTwo = () =>{
+//   const deltTwo = this.state.hideUserTwo;
+//   this.setState({
+//     hideUserTwo: ! deltTwo,
+//   })
+// }
+//
+// userThree = () =>{
+//   const deltThree = this.state.hideUserThree;
+//   this.setState({
+//     hideUserThree: !deltThree,
+//   })
+// }
+
+//////////////////////////////////
+
+// { this.state.hideUserOne === true ?
+//   <div>
+//     <Person click={this.switchNameHandler.bind(this, 'dawaj')} name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+//     <button onClick={this.userOne}>Delete Redwan</button>
+//   </div> : null
+// }
+//
+//
+// { this.state.hideUserTwo === true ?
+//   <div>
+//     <Person changed={this.nameChangedHandler} name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+//     <button onClick={this.userTwo}>Delete Manu</button>
+//   </div> : null
+// }
+//
+// {this.state.hideUserThree === true ?
+//   <div>
+//     <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+//     <button onClick={this.userThree}>Delete Jawad</button>
+//   </div> : null
+// }
---
+++ src/Person/Person.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './Person.css';
+
+const Person = (props) =>{
+  return(
+    <div className="Person">
+      <p onClick={props.click}>I am {props.name} and I am {props.age} years old</p>
+      <input type="text" onChange={props.changed} value={props.name}/>
+    </div>
+  )
+}
+
+export default Person;
